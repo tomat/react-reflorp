e25429b4faf3a1af478946d05fd37b77caf606a0
jens.paulin <jens.paulin@familjelivmedia.se>
2016-05-15 13:17:14 +0200

    added debounce helper

---
+++ src/utils/debounce.js
@@ -0,0 +1,18 @@
+export default function debounce(func, wait, immediate) {
+  let timeout;
+  return function (...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) {
+        func.apply(context, args);
+      }
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) {
+      func.apply(context, args);
+    }
+  };
+}
